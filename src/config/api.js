5b9ce252f323afbf805a660ff51b6df243b4e689
fungleo <web@fengcms.com>
2017-05-23 17:56:17 +0800

    将接口请求工具从superagent更换为axios

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // 配置API接口地址
-var root = process.env.API_ADDR
-// 引用superagent
-var request = require('superagent')
+var root = process.env.API_ROOT
+// 引用axios
+var axios = require('axios')
 // 自定义判断元素类型JS
 function toType (obj) {
   return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
@@ -14,9 +14,10 @@ function filterNull (o) {
     }
     if (toType(o[key]) === 'string') {
       o[key] = o[key].trim()
-      if (o[key].length === 0) {
-        delete o[key]
-      }
+    } else if (toType(o[key]) === 'object') {
+      o[key] = filterNull(o[key])
+    } else if (toType(o[key]) === 'array') {
+      o[key] = filterNull(o[key])
     }
   }
   return o
@@ -27,50 +28,56 @@ function filterNull (o) {
   https://cnodejs.org/api/v1 的接口，如果是其他接口
   需要根据接口的参数进行调整。参考说明文档地址：
   https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
+  主要是，不同的接口的成功标识和失败提示是不一致的。
+  另外，不同的项目的处理方法也是不一致的，这里出错就是简单的alert
 */
-function apiBase (method, url, params, success, failure) {
-  var r = request(method, url).type('text/plain')
+
+function apiAxios (method, url, params, success, failure) {
   if (params) {
     params = filterNull(params)
-    if (method === 'POST' || method === 'PUT') {
-      if (toType(params) === 'object') {
-        params = JSON.stringify(params)
-      }
-      r = r.send(params)
-    } else if (method === 'GET' || method === 'DELETE') {
-      r = r.query(params)
-    }
   }
-  r.end(function (err, res) {
-    if (err) {
-      window.alert('api error, HTTP CODE: ' + res.status)
-      return
-    };
-    if (res.body.success === true) {
+  axios({
+    method: method,
+    url: url,
+    data: method === 'POST' || method === 'PUT' ? params : null,
+    params: method === 'GET' || method === 'DELETE' ? params : null,
+    baseURL: root,
+    withCredentials: false
+  })
+  .then(function (res) {
+    if (res.data.success === true) {
       if (success) {
-        success(res.body)
+        success(res.data)
       }
     } else {
       if (failure) {
-        failure(res.body)
+        failure(res.data)
       } else {
-        window.alert('error: ' + JSON.stringify(res.body))
+        window.alert('error: ' + JSON.stringify(res.data))
       }
     }
   })
+  .catch(function (err) {
+    let res = err.response
+    if (err) {
+      window.alert('api error, HTTP CODE: ' + res.status)
+      return
+    }
+  })
 }
+
 // 返回在vue模板中的调用接口
 export default {
   get: function (url, params, success, failure) {
-    return apiBase('GET', root + '/' + url, params, success, failure)
+    return apiAxios('GET', url, params, success, failure)
   },
   post: function (url, params, success, failure) {
-    return apiBase('POST', root + '/' + url, params, success, failure)
+    return apiAxios('POST', url, params, success, failure)
   },
   put: function (url, params, success, failure) {
-    return apiBase('PUT', root + '/' + url, params, success, failure)
+    return apiAxios('PUT', url, params, success, failure)
   },
   delete: function (url, params, success, failure) {
-    return apiBase('DELETE', root + '/' + url, params, success, failure)
+    return apiAxios('DELETE', url, params, success, failure)
   }
 }
